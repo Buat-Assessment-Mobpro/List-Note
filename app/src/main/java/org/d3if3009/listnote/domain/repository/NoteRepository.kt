15c2c5400ecbce467d27a41731c9d80b0004672b
fahritech04 <fahricoding@gmail.com>
2024-05-11 20:02:02 +0700

    Menambahkan Model Note

---
+++ app/src/main/java/org/d3if3009/listnote/domain/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package org.d3if3009.listnote.domain.repository
+
+import com.example.notesappcompose.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+
+    fun getNotes() : Flow<List<Note>>
+
+    suspend fun getNoteById(id:Int) : Note?
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+}
